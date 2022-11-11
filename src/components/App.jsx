15aa1762068366a308470b154f91bcc2e3b79746
VasylMartin <vasiliymartiniouk@gmail.com>
2022-11-11 21:18:14 +0200

    HWR4 T1 Done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,46 +1,50 @@
-import React from "react";
+import {useState} from "react";
 import { Statistics } from "./Statistics/Statistics";
 import { FeedbackOptions } from "./FeedbackOptions/FeedbackOptions";
 import { Section } from "./Section/Section";
 import { Notification } from "./Notification/Notification";
 
-class App extends React.Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
-  };
+export default function App() {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const optionLabels = Object.keys({ good, neutral, bad });
 
 
-leaveFeedback = option => {
-  this.setState(prevState => {
-    return {
-      [option]: prevState[option] + 1
-    }
-  })
+const leaveFeedback = option => {
+  if(option === 'good'){
+    setGood(prevState => prevState + 1)
+  }
+
+  if(option === 'neutral'){
+    setNeutral(prevState => prevState + 1)
+  }
+
+  if(option === 'bad'){
+    setBad(prevState => prevState + 1)
+  }
 }
 
-
-  render() {
-    let countTotalFeedback = this.state.good + this.state.neutral + this.state.bad
+    let countTotalFeedback = good + neutral + bad
     let countPositiveFeedbackPercentage = Math.round(
-      (this.state.good / countTotalFeedback) * 100
+      (good / countTotalFeedback) * 100
       )
 
     return(
       <Section title="Please leave feedback">
         <FeedbackOptions 
-          options={Object.keys(this.state)}
-          onLeaveFeedback={this.leaveFeedback}
+          options={optionLabels}
+          onLeaveFeedback={leaveFeedback}
         />
         <h2>Statistics</h2>
           {countTotalFeedback === 0 ? (
             <Notification message="There is no feedback" />
           ) : (
         <Statistics 
-          good={this.state.good}
-          neutral={this.state.neutral}
-          bad={this.state.bad}
+          good={good}
+          neutral={neutral}
+          bad={bad}
           total={countTotalFeedback}
           positiveFeedbackPercentage={countPositiveFeedbackPercentage}
         />
@@ -48,7 +52,4 @@ leaveFeedback = option => {
       }
       </Section>
     )
-  }
 }
-
-export default App;
